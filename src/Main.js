c8b0848847da3eccea0a24e1e049588c47428b9a
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-22 14:31:32 -0400

    Add sidebar, notelist, and noteform components

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
+
+
+
 const Main = () => {
     return (
         <div className="Main">
-            Main!
+            <Sidebar />
+            <NoteList />
+            <NoteForm />
         </div>
     )
 }
