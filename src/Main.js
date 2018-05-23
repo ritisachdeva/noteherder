42ae3051b4e1613eeb5fe4a2061ea414d52102c0
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-23 15:52:09 -0400

    Edit existing notes

--- src/Main.js
+++ src/Main.js
@@ -1,69 +1,78 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-
-
-class Main extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            currentNote: this.blankNote(),
-            notes: [
-                {
-                    id: 1,
-                    title: 'JS thoughts',
-                    body: 'I just love JS so much.',
-                },
-                {
-                    id: 2,
-                    title: 'Breakfast',
-                    body: 'FOR IT!',
-                },
-            ],
-        }
+class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentNote: this.blankNote(),
+      notes: [
+        {
+          id: 1,
+          title: 'JS thoughts',
+          body: 'I just love JS so much.',
+        },
+        {
+          id: 2,
+          title: 'Breakfast',
+          body: 'FOR IT!',
+        },
+      ],
     }
+  }
 
-    blankNote = () => {
-        return{
-            id: null,
-            title: '',
-            body: '', 
-        }
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
     }
+  }
 
-    setCurrentNote = (note) => {
-        this.setState({ currentNote: note})
-    }
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
 
-    resetCurrentNote = () => {
-        this.setCurrentNote(this.blankNote())
-    }
+  resetCurrentNote = () => {
+    this.setCurrentNote(this.blankNote())
+  }
 
+  saveNote = (note) => {
+    const notes = [...this.state.notes]
 
-    render() {
+    const i = notes.findIndex(currentNote => currentNote.id === note.id)
+    notes[i] = note
+
+    this.setState({ notes, currentNote: note })
+  }
+
+  render() {
     return (
-        <div 
-            className="Main"
-            style={style}
-        >
-            <Sidebar resetCurrentNote={this.resetCurrentNote} />
-            <NoteList 
-                notes={this.state.notes}
-                setCurrentNote={this.setCurrentNote}
-            />
-            <NoteForm currentNote={this.state.currentNote}/>
-        </div>
+      <div
+        className="Main"
+        style={style}
+      >
+        <Sidebar resetCurrentNote={this.resetCurrentNote} />
+        <NoteList
+          notes={this.state.notes}
+          setCurrentNote={this.setCurrentNote}
+        />
+        <NoteForm
+          currentNote={this.state.currentNote}
+          saveNote={this.saveNote}
+        />
+      </div>
     )
-}
+  }
 }
 
 const style = {
-    display: 'flex',
-    height: '100vh',
-    alignItems: 'stretch',
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'stretch',
 }
 
 export default Main
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,31 +2,39 @@ import React from 'react'
 
 import './NoteForm.css'
 
-const NoteForm = ({currentNote}) => {
-    return( 
-        <div className="NoteForm">
-          <div className="form-actions">
-            <button type="button">
-              <i className="far fa-trash-alt"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-                value={currentNote.title}
-              />
-            </p>
-            
-            <textarea 
-                name="body"
-                value ={currentNote.body}
-            ></textarea>
-          </form>
-        </div>
-    )
+const NoteForm = ({ currentNote, saveNote }) => {
+  const handleChanges = (ev) => {
+    const note = {...currentNote}
+    note[ev.target.name] = ev.target.value
+    saveNote(note)
+  }
+
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+            value={currentNote.title}
+            onChange={handleChanges}
+          />
+        </p>
+
+        <textarea
+          name="body"
+          value={currentNote.body}
+          onChange={handleChanges}
+        ></textarea>
+      </form>
+    </div>
+  )
 }
 
 export default NoteForm
