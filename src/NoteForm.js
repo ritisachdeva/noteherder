5267ae8ead8f60fa1dd2edf8169fb2645d6efb4e
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-23 14:35:18 -0400

    Dynamically render notes from array

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -7,7 +7,7 @@ const NoteForm = () => {
         <div className="NoteForm">
           <div className="form-actions">
             <button type="button">
-              <i className="fa fa-trash-o"></i>
+              <i className="far fa-trash-alt"></i>
             </button>
           </div>
           <form>
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,89 +1,35 @@
 import React from 'react'
+
 import './NoteList.css'
+import Note from './Note'
 
-//const NoteList = () => 
-class NoteList extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            notes:[
-                    {title: "Kohlrabi welsh", content:"Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic."},
-                    {title: "Dandelion cucumber", content:"Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini."},
-                    {title: "Prairie turnip", content: "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip."}
-            ]
-        }
-    }
+const NoteList = () => {
+    const notes=[
+        {
+            id: 1,
+            title: 'JS thoughts',
+            body: 'I just love JS so much.',
+        },
+        {
+            id: 2,
+            title: 'Breakfast',
+            body: 'FOR IT!',
+        },
+        {
+            id: 3,
+            title: 'Class notes',
+            body: 'React is spiffy'
+        },
+    ]
 
-    
-    render(){
-        console.log(this.state.notes)
-        return (
-            <div className="NoteList">
-              <h3>Notes</h3>
-              <ul id="notes">
-                {/* <a class="active">
-                  <li>
-                    <div class="note">
-                      <div class="note-title">
-                        Kohlrabi welsh
-                      </div>
-                      <div class="note-body">
-                        <p>
-                          Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                </a>
-                <a>
-                  <li>
-                    <div class="note">
-                      <div class="note-title">
-                        Dandelion cucumber
-                      </div>
-                      <div class="note-body">
-                        <p>
-                          Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                </a>
-                <a>
-                  <li>
-                    <div class="note">
-                      <div class="note-title">
-                        Prairie turnip
-                      </div>
-                      <div class="note-body">
-                        <p>
-                          Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                </a> */}
-
-                {this.state.notes.map(note =>
-                <a key={note.title}>
-                  <li>
-                    <div className="note">
-                      <div className="note-title">
-                        {note.title}
-                      </div>
-                      <div className="note-body">
-                        <p>
-                            {note.content}
-                        </p>
-                      </div>
-                    </div>
-                  </li>
-                </a>
-                )}
-              </ul>
-            </div>
-        )
-    }
+  return (
+    <div className="NoteList">
+      <h3>Notes</h3>
+      <ul id="notes">
+        { notes.map(note => <Note note={note} />) }
+      </ul>
+    </div>
+  )
 }
 
 export default NoteList
