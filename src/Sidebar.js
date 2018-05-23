b427a30f6df6907fb5d8155ddfdc23ff5b174b6d
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-23 13:52:02 -0400

    Handle mouse events in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,93 +1,120 @@
-import React from 'react'
+
+import React, { Component } from 'react'
 
 import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
-import './Sidebar.css'
 
-const Sidebar = () => {
+class Sidebar extends Component {
+  state = {
+    newIconHovered: false,
+  }
+
+  handleMouseEnter() {
+    this.setState({ newIconHovered: true })
+  }
+
+  handleMouseLeave() {
+    this.setState({ newIconHovered: false })
+  }
+
+  render() {
     return (
-        <nav 
-            className="Sidebar"
-            //style = {styles.sidebar}
+      <nav
+        className="Sidebar"
+        style={styles.sidebar}
+      >
+        <div
+          className="logo"
+          style={styles.logo}
         >
-            <div 
-                className="logo"
-                //style= {styles.logo}
-            >
-            <img 
-                src={quill} 
-                alt="Noteherder" 
-                //style={styles.logoImg}
-            />
-            </div>
-
-          <a 
-            className="new-note" 
-            href="/notes"
-            //style={styles.newNote}
-          >
-            <img 
-                src={newHover} 
-                alt="New note"
-                //style={styles.img} 
-            />
-            <img 
-                className="outline" 
-                src={newIcon} alt="New note" 
-            />
-          </a>
-          <div className="SignOut">
-            <button>
-              <i className="fa fa-sign-out"></i>
-            </button>
-          </div>
-        </nav>
+          <img
+            src={quill}
+            alt="Noteherder"
+            style={styles.logoImg}
+          />
+        </div>
+        <a
+          className="new-note"
+          href="/notes"
+          style={styles.newNote}
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <img
+            src={newHover}
+            alt="New note"
+            style={styles.newNoteImg}
+          />
+          <img
+            className="outline"
+            src={newIcon}
+            alt="New note"
+            style={{
+              ...styles.newNoteImg,
+              opacity: this.state.newIconHovered ? 0 : 1
+            }}
+          />
+        </a>
+        <div
+          className="SignOut"
+          style={styles.signOut}
+        >
+          <button style={styles.button}>
+            <i
+              className="fa fa-sign-out"
+              style={styles.buttonIcon}
+            ></i>
+          </button>
+        </div>
+      </nav>
     )
+  }
 }
 
-// const styles = {
-//     sidebar: {
-//         width: '6rem',
-//         backgroundColor: '#f3f3f3',
-//         padding: '0.5rem 0',
-//         display: 'flex',
-//         flexDirection: 'column',
-//         alignItems: 'center',
-//     },
-//     logo: {
-//         fontFamily: '"Fauna One"',
-//         color: '#666',
-//         fontSize: '3rem',
-//     },
-//     logoImg: {
-//         width: '3rem',
-//         paddingLeft: '0.4rem',
-//     },
-//     newNote: {
-//         marginTop: '2rem',
-//         position: 'relative',
-//         width: '4rem',
-//     },
-//     newNoteImg: {
-//         position: 'absolute',
-//         left: '0',
-//         width: '100%',
-//     },
-//     signOut: {
-//         position: 'absolute',
-//         bottom: '1rem',
-//     },
-//     button: {
-//         backgroundColor: 'transparent',
-//         border: '0',
-//         color: '#008BF8',
-//         cursor: 'pointer',
-//         outline: 'none',
-//     },
-//     buttonIcon: {
-//         fontSize: '2rem',
-//     }
-//}
+const styles = {
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  },
+  newNote: {
+    marginTop: '2rem',
+    position: 'relative',
+    width: '4rem',
+  },
+  newNoteImg: {
+    position: 'absolute',
+    left: '0',
+    width: '100%',
+    transition: 'opacity 0.25s ease-in-out',
+  },
+  signOut: {
+    position: 'absolute',
+    bottom: '1rem',
+  },
+  button: {
+    backgroundColor: 'transparent',
+    border: '0',
+    color: '#008bf8',
+    cursor: 'pointer',
+    outline: 'none',
+  },
+  buttonIcon: {
+    fontSize: '2rem',
+  },
+}
 
 export default Sidebar
