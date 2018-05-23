5c9dc92f47fcd9f01a95880b37379f67f74bc907
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-23 13:23:28 -0400

    Layout for noted

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -5,7 +5,22 @@ import './NoteForm.css'
 const NoteForm = () => {
     return( 
         <div className="NoteForm">
-            NoteForm
+          <div className="form-actions">
+            <button type="button">
+              <i className="fa fa-trash-o"></i>
+            </button>
+          </div>
+          <form>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+              />
+            </p>
+            
+            <textarea name="body"></textarea>
+          </form>
         </div>
     )
 }
