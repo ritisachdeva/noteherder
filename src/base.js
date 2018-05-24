c9ca82f14cab1aa323ad8d7e2b64955e231b18e7
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-24 14:34:26 -0400

    Add firebase realtime database

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDm71EEkTi3Tz5sm-wYitHILT42tB-QUiU",
+    authDomain: "noteherder-e5896.firebaseapp.com",
+    databaseURL: "https://noteherder-e5896.firebaseio.com",
+    projectId: "noteherder-e5896",
+    storageBucket: "noteherder-e5896.appspot.com",
+    messagingSenderId: "672274357192"
+  };
+  firebase.initializeApp(config);
